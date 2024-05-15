672a77f0c1ff2bd7ff03f9cdb402d6e0acc15654
Rory Salvatore <rory121212@gmail.com>
2024-05-16 02:36:48 +0300

    ip/port of the centralized log service from the arguments

--- README.md
+++ README.md
@@ -17,12 +17,14 @@ python src/server.py
 - Install docker
 - `docker-compose up --build`
 - Link to local [Kibana](http://localhost:5601) (port 5601)
-- 
+
+#### All services is online! 
 
 ## Work process
 ### Tools used
 Client testing tool - [client.py](external_sources/client/client.py) (Run: `python external_sources/client/client.py`)
 
-## Assumptions
+### Assumptions
 - Log message maximum length is 4096 (configurable). To avoid creation of protocol between client and server.
 - The timestamp is added automatically.
+- You can configure the ip/port of the centralized log service from the arguments. (See `python app.py --help`)
--- src/server.py
+++ src/server.py
@@ -1,4 +1,6 @@
 import socket
+
+from src.args_parser import parse_args
 from src.config.config import config
 from src.logstash_logger.logstash_client import LogstashClient
 
@@ -39,12 +41,25 @@ def start_server(host: str, port: int, logstash: LogstashClient) -> None:
                         break
 
 
+def update_config():
+    args = parse_args()
+    logstash_config = config["logstash"]
+    if args.ip is not None:
+        logstash_config["host"] = args.ip
+    if args.port is not None:
+        logstash_config["port"] = args.port
+
+
 def run():
+    update_config()
     server_config = config["server"]
     logstash_config = config["logstash"]
+
     logstash_client = LogstashClient(logstash_config["host"], logstash_config["port"])
     logstash_client.connect()
+
     start_server(server_config["host"], server_config["port"], logstash_client)
+
     logstash_client.close()
 
 
